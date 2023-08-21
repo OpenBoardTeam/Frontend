8ca19b0c7d408556675db5d531cfc28ff0d7d6f2
EunHyeokJung <silverhyeok.dev@gmail.com>
2023-08-22 04:56:37 +0900

    Add: Footer.jsx
    - Licence.jsx 분할 (유지보수)

---
+++ src/componenets/Footer/Licence.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { StyledLink } from "../../style/theme";
+
+const name = "MIT Licence";
+const url = "https://opensource.org/license/mit/";
+
+const LicenceLink = styled(StyledLink)`
+    color: #63a1ff;
+    &:visited {
+        color: #bc43be;
+    }
+`;
+
+const Licence = () => {
+    return (
+        <>
+            <p>
+                Copyright &copy; {new Date().getFullYear()} GitBoard. All rights
+                reserved. Licensed under the{" "}
+                <LicenceLink href={url} target="_blank">
+                    {name}
+                </LicenceLink>
+                .
+            </p>
+        </>
+    );
+};
+
+export default Licence;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
+import React from "react";
+import theme from "../style/theme";
+import styled, { ThemeProvider } from "styled-components";
+import Footer from "../componenets/Footer/Footer";
+
+const StyledHome = styled.div`
+    background-color: ${(props) => props.theme.primaryColor.background.black};
+    color: white;
+`;
+
+const Main = styled.div`
+    width: 100%;
+    height: 60vh;
+`;
+
+const Categories = styled.div`
+    width: 100%;
+`;
+
+const footer = styled.div`
+    width: 100%;
+`;
+
 const Home = () => {
     return (
-        <>
-            <p>Home</p>
-        </>
+        <ThemeProvider theme={theme}>
+            <StyledHome>
+                <Footer></Footer>
+            </StyledHome>
+        </ThemeProvider>
     );
 };
 
