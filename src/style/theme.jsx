6e4144fc8cd3eb0480f4422af70a347a7b1a1ff3
EunHyeokJung <silverhyeok.dev@gmail.com>
2023-08-22 01:44:20 +0900

    Add: theme.jsx

---
+++ src/style/theme.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const primaryColor = {
+    background: {
+        black: Color("#07030B"),
+        textArea: Color("1C1920"),
+    },
+
+    white: Color("#FFFFFF"),
+    black: Color("#141414"),
+    darkGray: Color("#23212D"),
+};
+
+const theme = {
+    primaryColor,
+};
+
+export default theme;
