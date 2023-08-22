c80ed69b29fbee44a2c82d41f68d63945af25693
EunHyeokJung <silverhyeok.dev@gmail.com>
2023-08-23 05:19:41 +0900

    Test: 테스트용 TestMain 코드 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,31 @@ import React from "react";
 import theme from "../style/theme";
 import styled, { ThemeProvider } from "styled-components";
 import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 const StyledHome = styled.div`
     background-color: ${(props) => props.theme.primaryColor.background.black};
     color: white;
 `;
 
-const Main = styled.div`
-    width: 100%;
-    height: 60vh;
-`;
-
 const Categories = styled.div`
     width: 100%;
 `;
 
-const footer = styled.div`
+const TestMain = styled.div`
     width: 100%;
+    height: 90vh;
+    background: url("/images/PcHome.png") center;
+    background-repeat: no-repeat;
+    background-size: cover;
 `;
 
 const Home = () => {
     return (
         <ThemeProvider theme={theme}>
+            <Header></Header>
             <StyledHome>
+                <TestMain></TestMain>
                 <Footer></Footer>
             </StyledHome>
         </ThemeProvider>
