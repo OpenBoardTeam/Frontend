f2192ec164aac14f26ff00d460cf0fe97703b511
Lihan <96524080+Lihan00@users.noreply.github.com>
2023-08-24 17:08:07 +0900

    Add, Feat, Design: Profile 페이지 기초 틀 작업

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,38 @@
+import React from "react";
+import "./Profile.css";
+import ProfileMy from "./ProfileMy";
+import { useState } from "react";
+
 const Profile = () => {
+    const [menu, setMenu] = useState(0)
+    const [state, setState] = useState(0)
     return (
         <>
-            <p>Profile</p>
+            <div className="postheader">
+
+            </div>
+            <div className="profilebody">
+                <div className={menu === 0 ? "profilemenuclose" : "profilemenuopen"}>
+                    {menu === 0 && (
+                        <div className="profilemenubutton" onClick={() => setMenu(1)}>=</div>
+                    )}
+                    {menu === 1 && (
+                        <>
+                            <div className="profilemenubutton" onClick={() => setMenu(0)}>{`<`}</div>
+                            <div className={state !== 0 ? "buttonmy" : "buttonmyclick"} onClick={() => setState(0)}>My Profile</div>
+                            <div className={state !== 1 ? "buttonproject" : "mybuttonclick"} onClick={() => setState(1)}>Project Detail</div>
+                            <div className={state !== 2 ? "buttoninterest" : "mybuttonclick"} onClick={() => setState(2)}>Interest</div>
+                        </>
+                    )}
+                    
+                </div>
+                <div className="profilemain">
+                    {state === 0 && <ProfileMy/>}
+                </div>
+            </div>
+            <div className="postfooter">
+
+            </div>
         </>
     );
 };
