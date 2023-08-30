5a66314b20b821a3ee5ad19007966c16486a33cd
Lihan <96524080+Lihan00@users.noreply.github.com>
2023-08-30 10:55:36 +0900

    Design: 변경된 디자인 수정

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,34 +1,32 @@
 import React from "react";
 import "./Profile.css";
-import ProfileMy from "./ProfileMy";
-import { useState } from "react";
 
 const Profile = () => {
-    const [menu, setMenu] = useState(0)
-    const [state, setState] = useState(0)
     return (
         <>
             <div className="postheader">
 
             </div>
             <div className="profilebody">
-                <div className={menu === 0 ? "profilemenuclose" : "profilemenuopen"}>
-                    {menu === 0 && (
-                        <div className="profilemenubutton" onClick={() => setMenu(1)}>=</div>
-                    )}
-                    {menu === 1 && (
-                        <>
-                            <div className="profilemenubutton" onClick={() => setMenu(0)}>{`<`}</div>
-                            <div className={state !== 0 ? "buttonmy" : "buttonmyclick"} onClick={() => setState(0)}>My Profile</div>
-                            <div className={state !== 1 ? "buttonproject" : "mybuttonclick"} onClick={() => setState(1)}>Project Detail</div>
-                            <div className={state !== 2 ? "buttoninterest" : "mybuttonclick"} onClick={() => setState(2)}>Interest</div>
-                        </>
-                    )}
-                    
+                <div className="mybody">
+                    <div className="myleft">
+                        <div className="myedit">Edit Profile</div>
+                        <div className="myimg"></div>
+                        <div className="myname">Lihan</div>
+                        <div className="myid">Lihan030</div>
+                        <div className="mybookmark">7 bookmark</div>
+                    </div>
+                    <div className="myright">
+                        <div className="myprojecttitle">Project List</div>
+                        <div className="myprojectlist">
+
+                        </div>
+                        <div className="myintro">Intro</div>
+                        <div className="mycontent">contents</div>
+                        <div className="mybadge">Badge</div>
+                    </div>
                 </div>
-                <div className="profilemain">
-                    {state === 0 && <ProfileMy/>}
-                </div>
+                <div className="mywith">withdrawal</div>
             </div>
             <div className="postfooter">
 
